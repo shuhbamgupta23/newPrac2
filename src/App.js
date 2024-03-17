bf5cbb6c4aa3ffcb20fad1dd06fb8da38525a87d
Shubham <mrjanuaryx@gmail.com>
2024-03-17 14:50:25 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const App = () => {
+
+  const [temp, setTemp] = useState(0);
+
+  const handleChange = (e)=>{
+    setTemp(parseFloat(e.target.value))
+  }
+  const tofarh = (val) => {
+    return Math.round(((val * 9) / 5 + 32) * 100) / 100;
+  };
+  const tokelvin = (val) => {
+    return Math.round((val + 273.15) * 100) / 100;
+  };
+  console.log("hell");
+  return (
+    <div>
+      <form>
+        <input
+          data-testid="input-id"
+          name="input"
+          type="number"
+          value={temp}
+          onChange={(e) => handleChange(e)}
+        />
+        <label for="input">°C</label>
+      </form>
+      <p data-testid="output">
+        {temp}°C is {tofarh(temp)}°F and {tokelvin(temp)}K.
+      </p>
+    </div>
+  );
+};
+
+export default App;
